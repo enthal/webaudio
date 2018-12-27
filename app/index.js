4fbdefb1056227528aea637ab9c508d01175dc00
Tim James <tjames@tjames.com>
2018-12-27 13:47:24 -0800

    Lil refactor

--- app/index.js
+++ app/index.js
@@ -5,23 +5,28 @@ module.exports =
 (selector='body') => {
   const audioCtx = new (window.AudioContext || window.webkitAudioContext)();  // TODO: by browser policy, must (I THINK!) be created in a click handler or all audio is muted (ok for now on desktop Chrome, but to change in next Chrome release)
 
-  const scheduler = require('./scheduler')(audioCtx);
-  const master = makeMaster(audioCtx);
+  const GO = () => {
+    const scheduler = require('./scheduler')(audioCtx);
+    const master = makeMaster(audioCtx);
+    const topEl = require('./controls').attachTopControls(audioCtx, scheduler, master);
 
-  const topEl = require('./controls').attachTopControls(audioCtx, scheduler, master);
+    const drums = makeDrums(master, [ 69-12, 69-7, 69 ]);  // 69 == MIDI A4; 12 == octave note width;
+    const beatGridEl = require('./beatGrid')(scheduler, drums);
 
-  const instruments = require('./instruments');
-  const drums = [ 69-12, 69-7, 69 ]  // 69 == MIDI A4; 12 == octave note width;
-    .map( midiNote => instruments.makeSimpleOscillatorDrum(audioCtx, master, midiNote) );
+    el.select(selector, [topEl, beatGridEl]);
+  }
 
-  const beatGridEl = require('./beatGrid')(scheduler, drums);
+  const makeMaster = () => {
+    const master = audioCtx.createGain();
+    master.gain.value = 0.2;
+    master.connect(audioCtx.destination);
+    return master;
+  }
 
-  el.select(selector, [topEl, beatGridEl]);
-}
+  const makeDrums = (output, midiNotes) =>
+    midiNotes.map( midiNote =>
+      require('./instruments')
+        .makeSimpleOscillatorDrum(audioCtx, output, midiNote) );
 
-const makeMaster = audioCtx => {
-  const master = audioCtx.createGain();
-  master.gain.value = 0.2;
-  master.connect(audioCtx.destination);
-  return master;
+  GO();
 }
